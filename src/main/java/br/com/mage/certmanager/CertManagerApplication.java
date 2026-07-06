874c16bb36bdeb273cbb44269f6227bbdcee0335
D. Gabrielz <mendesdouglasgabriel30@gmail.com>
2026-07-06 00:31:30 -0300

    chore: inital project setup

---
+++ src/main/java/br/com/mage/certmanager/CertManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.mage.certmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mage/certmanager/CertManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mage.certmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
